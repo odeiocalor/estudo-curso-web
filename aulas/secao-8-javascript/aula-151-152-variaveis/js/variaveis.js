7d3975c53eaf8be6db727984217d2b8a6260d598
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-03 16:33:51 -0300

    adição aula-151-152-variaveis

---
+++ aulas/secao-8-javascript/aula-151-152-variaveis/js/variaveis.js
@@ -0,0 +1,18 @@
+// string
+
+var texto = 'Curso de JavaScript'
+
+// number
+
+var numeroInteiro = -7
+var numeroFracionado = 10.75
+
+// boolean
+
+var teste = true // true -> verdadeiro / false -> falso
+
+alert(numeroFracionado) // abrir um dialog
+
+document.write(texto)
+
+console.log(numeroInteiro)
