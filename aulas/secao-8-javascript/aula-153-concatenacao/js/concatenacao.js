05dc1413b26f988e168174e7c36c8aa224f65033
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-03 16:55:25 -0300

    adição aula-153-concatenacao

---
+++ aulas/secao-8-javascript/aula-153-concatenacao/js/concatenacao.js
@@ -0,0 +1,8 @@
+var nome = prompt('Digite o seu nome: ')
+var idade = prompt('Digite sua idade: ')
+var idade2 = 50
+
+console.log(idade)
+console.log(idade2)
+
+document.write('<h1>Olá ' + nome + ', tudo bem? Estou vendo aqui que você possui ' + idade + ' anos.</h1>')
