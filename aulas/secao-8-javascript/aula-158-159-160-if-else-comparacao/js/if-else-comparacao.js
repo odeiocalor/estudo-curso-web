7ca11ef3c375b575bc9e7698a77a3b2024a1b875
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-04 16:11:39 -0300

    adição aula-158-159-160-if-else-comparacao

---
+++ aulas/secao-8-javascript/aula-158-159-160-if-else-comparacao/js/if-else-comparacao.js
@@ -0,0 +1,6 @@
+// Insira comparações condicionais dentro dos parênteses
+if (2 !== 2) {
+    document.write('Entrou dentro do bloco if (verdadeiro)')
+} else {
+    document.write('Entrou dentro do bloco else (falso)')
+}
