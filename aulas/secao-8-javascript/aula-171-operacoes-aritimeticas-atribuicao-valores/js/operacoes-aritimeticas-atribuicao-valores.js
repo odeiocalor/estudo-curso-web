d84c0a5fae515023b24af64db4cd820d0a20884d
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-05 16:22:50 -0300

    adição aula-171-operacoes-aritimeticas-atribuicao-valores

---
+++ aulas/secao-8-javascript/aula-171-operacoes-aritimeticas-atribuicao-valores/js/operacoes-aritimeticas-atribuicao-valores.js
@@ -0,0 +1,17 @@
+var teste = 10
+var testeString = 'Olá '
+
+document.write('teste = ' + teste + '<br>')
+//teste = teste + 5
+teste += 5
+document.write('teste += 5: ' + teste + '<br>')
+teste -= 5
+document.write('teste -= 5: ' + teste + '<br>')
+teste *= 5
+document.write('teste *= 5: ' + teste + '<br>')
+teste /= 5
+document.write('teste /= 5: ' + teste + '<br>')
+teste %= 5
+document.write('teste %= 5: ' + teste + '<br>')
+testeString += 'Jorge'
+document.write(testeString)
