ca813709de0d9c89d93daaff2ba95f33d62e3ee4
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-05 16:33:12 -0300

    adição aula-172-precedencia-de-execucao-operacoes-aritimeticas

---
+++ aulas/secao-8-javascript/aula-172-precedencia-de-execucao-operacoes-aritimeticas/js/precedencia-de-execucao-operacoes-aritimeticas.js
@@ -0,0 +1,3 @@
+document.write('O resultado de (11 + 5 - 5 * 2 / 2) é: ' + (11 + 5 - 5 * 2 / 2) + '<br>')
+
+document.write('O resultado de (11 + (5 - 5 * 2) / 2) é: ' + (11 + (5 - 5 * 2) / 2) + '<br>')
