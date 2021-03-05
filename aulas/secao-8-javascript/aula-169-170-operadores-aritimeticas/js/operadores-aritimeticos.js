382b61cef1e54f5a4b5e662f648f94308d4dac65
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-05 16:10:29 -0300

    adição aula-169-170-operacoes-aritimeticas

---
+++ aulas/secao-8-javascript/aula-169-170-operadores-aritimeticas/js/operadores-aritimeticos.js
@@ -0,0 +1,18 @@
+var num1 = parseInt(prompt('Digite o valor de num1'))
+var num2 = parseInt(prompt('Digite o valor de num2'))
+
+document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2) + '<br>')
+
+document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + '<br>')
+
+document.write('A múltiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + '<br>')
+
+document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + '<br>')
+
+document.write('A módulo (resto da divisão) entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br>')
+
+document.write('A incremento (anterior) de ' + num1 + ' é: ' + (++num1) + '<br>')
+document.write('A incremento (posterior) de ' + num1 + ' é: ' + (num1++) + '->' + num1 + '<br>')
+
+document.write('A decremento (anterior) de ' + num1 + ' é: ' + (--num1) + '<br>')
+document.write('A decremento (posterior) de ' + num1 + ' é: ' + (num1--) + '->' + num1 + '<br>')
