b4131ed4c1b730dea81fdcdb9e7010857e5ff843
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-08 17:21:13 -0300

    Revert "Revert "adição aula-181-funcoes-math""
    This reverts commit 6482002e6d0313cb5bf9448c414577cf18163d60.

---
+++ aulas/secao-8-javascript/aula-181-funcoes-math/js/funcoes-math.js
@@ -0,0 +1,19 @@
+var x = 10.380
+
+// Arredonda o valor para cima
+document.write(Math.ceil(x))
+
+document.write('<br>')
+
+// Arredonda o valor para baixo
+document.write(Math.floor(x))
+
+document.write('<br>')
+
+// Arredonda o valor
+document.write(Math.round(x))
+
+document.write('<br>')
+
+// Fornece um número aleatório
+document.write(Math.random(0, 1))
