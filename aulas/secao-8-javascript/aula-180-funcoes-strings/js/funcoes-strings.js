c4c7fc26dfd638eb661ca70a2ecf5f7a4efd6e5d
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-08 16:36:43 -0300

    adição aula-180-funcoes-strings

---
+++ aulas/secao-8-javascript/aula-180-funcoes-strings/js/funcoes-strings.js
@@ -0,0 +1,40 @@
+var nome = 'Carlos Souza'
+var nomeEspaco = '  Carlos Souza  '
+
+// Propriedade length
+document.write(nome.length)
+
+document.write('<br>')
+
+// Propriedade charAt
+document.write(nome.charAt(5))
+
+document.write('<br>')
+
+// Propriedade indexOf
+document.write(nome.indexOf('u'))
+
+document.write('<br>')
+
+// Propriedade replace
+document.write(nome.replace('Souza', 'Alberto'))
+
+document.write('<br>')
+
+// Propriedade substr
+document.write(nome.substr(7, 11))
+
+document.write('<br>')
+
+// Propriedade toLowerCase
+document.write(nome.toLowerCase())
+
+document.write('<br>')
+
+// Propriedade toUpperCase
+document.write(nome.toUpperCase())
+
+document.write('<br>')
+
+// Propriedade trim
+document.write(nome.trim())
