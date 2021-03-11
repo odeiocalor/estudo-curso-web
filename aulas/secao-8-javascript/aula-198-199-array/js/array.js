b3ba43d8b9b63a8230ffb99a2bc099f3bc23f92e
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-11 16:08:47 -0300

    adição aula-198-199-array

---
+++ aulas/secao-8-javascript/aula-198-199-array/js/array.js
@@ -0,0 +1,19 @@
+// Ambos os modos geram o mesmo resultado
+// var lista_frutas = Array()
+var lista_frutas = []
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Morango'
+lista_frutas['x'] = 'Maçã'
+lista_frutas[3] = 'Uva'
+
+console.log(lista_frutas)
+
+console.log(lista_frutas[3])
+
+
+// Ambos os modos geram o mesmo resultado. 
+// var lista_marcas_carros = Array('BMW', 'Volkswagen', 'Fiat', 'Ferrari')
+var lista_marcas_carros = ['BMW', 'Volkswagen', 'Fiat', 'Ferrari']
+
+console.log(lista_marcas_carros)
