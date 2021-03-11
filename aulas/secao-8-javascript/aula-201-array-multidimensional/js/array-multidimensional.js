f0adb74203185a8e298802629850fc5c88727de7
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-11 16:48:30 -0300

    adição aula-201-array-multidimensional

---
+++ aulas/secao-8-javascript/aula-201-array-multidimensional/js/array-multidimensional.js
@@ -0,0 +1,10 @@
+var lista_coisas = []
+
+lista_coisas['frutas'] = ['Banana', 'Maçã', 'Morango', 'Uva']
+
+lista_coisas['pessoas'] = ['João', 'José', 'Maria', 'Alice']
+
+console.log(lista_coisas)
+
+document.write(lista_coisas['frutas'][3] + '<br>')
+document.write(lista_coisas['pessoas'][3] + '<br>')
