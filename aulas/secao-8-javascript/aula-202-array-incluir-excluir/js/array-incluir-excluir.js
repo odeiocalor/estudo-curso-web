fc1f8d0b9108d8a203b3133ce0d546f59adec998
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-11 17:44:51 -0300

    adição aula-202-array-incluir-excluir

---
+++ aulas/secao-8-javascript/aula-202-array-incluir-excluir/js/array-incluir-excluir.js
@@ -0,0 +1,27 @@
+var lista_frutas = ['Banana', 'Maçã']
+
+console.log(lista_frutas)
+
+// Incluir elemento no fim do array
+lista_frutas.push('Uva')
+
+// Incluir elemento no início do array
+lista_frutas.unshift('Morango')
+
+console.log(lista_frutas)
+
+// Excluir elemento no fim do array
+lista_frutas.pop('')
+
+// Excluir elemento no início do array
+lista_frutas.shift('')
+
+console.log(lista_frutas)
+
+var lista_coisas = []
+
+lista_coisas['pessoas'] = []
+lista_coisas['carros'] = []
+
+// Incluir elementos dentro de arrays multidimensionais. Todos os outros métodos funcionariam no mesmo molde.
+lista_coisas['pessoas'].push('José')
