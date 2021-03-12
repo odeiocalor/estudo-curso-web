3b1e5a623f99ea1a7c024ed9ee18c6e60cb03122
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-12 16:02:17 -0300

    adição aula-209-estrutura-de-repeticao-for

---
+++ aulas/secao-8-javascript/aula-209-estrutura-de-repeticao-for/js/estrutura-de-repeticao-for.js
@@ -0,0 +1,6 @@
+// No for, tanto a variavel, a condição e o controle são declarados como parâmetro inicial
+for (var x = 10; x >= 0; x--) {
+    document.write(x + '<br>')
+
+    // break e continue também podem ser usados, assim como no while e do while
+}
