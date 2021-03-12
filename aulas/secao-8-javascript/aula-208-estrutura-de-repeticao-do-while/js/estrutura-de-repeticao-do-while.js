7d718dc0eb29d6009361fcd2f962758c1dfc6c91
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-12 15:53:41 -0300

    adição aula-208-estrutura-de-repeticao-do-while

---
+++ aulas/secao-8-javascript/aula-208-estrutura-de-repeticao-do-while/js/estrutura-de-repeticao-do-while.js
@@ -0,0 +1,6 @@
+var x = 11
+
+// O Do while checa a condição apenas após a execução, sendo assim, é garantido que o loop será executado ao menos 1 vez.
+do {
+    document.write(x + '<br>')
+} while (x <= 10)
