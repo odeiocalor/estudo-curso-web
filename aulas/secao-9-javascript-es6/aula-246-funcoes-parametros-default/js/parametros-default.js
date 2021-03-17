68218963a412f8d3252b132606be1d95dfb61b2a
odeiocalor <carlosalbertojuvino@gmail.com>
2021-03-17 19:20:51 -0300

    adição aula-246-funcoes-parametros-default

---
+++ aulas/secao-9-javascript-es6/aula-246-funcoes-parametros-default/js/parametros-default.js
@@ -0,0 +1,6 @@
+function contarUmaHistoria(personagem = 'Fubá', atividade = 'correr no parque', nome_dono = 'Carlos') {
+    document.write(`Era uma vez um cachorro chamado ${personagem}, ele adorava ${atividade}, seu dono era o ${nome_dono} e eles viveram felizes para sempre`)
+}
+
+// contarUmaHistoria('Scooby', 'comer', 'Jorge')
+contarUmaHistoria(undefined, 'comer', 'Jorge')
