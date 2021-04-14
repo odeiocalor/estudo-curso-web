bf482b531a811a46246352d793e3c9d11dbd1148
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-14 18:05:14 -0300

    adição aula-301-echo-print

---
+++ aulas/secao-11-php7/aula-301-echo-print/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Echo e Print</title>
+	</head>
+	<body>
+		
+		<?php 
+			echo 'Comando echo<br>';
+			print 'Comando print<hr>';
+
+			// o print era originalmente uma função. Essa função retorna 1 caso a impressão ocorra, e 0 caso não ocorra
+			echo print ('Comando print<hr>');
+		?>
+
+	</body>
+</html>
