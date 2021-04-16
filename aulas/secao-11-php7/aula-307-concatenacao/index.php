e5ff41747ddbf528fe5fb8f7c744b89514eaec95
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-16 17:57:13 -0300

    adição aula-307-concatenacao

---
+++ aulas/secao-11-php7/aula-307-concatenacao/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Concatenação</title>
+	</head>
+	<body>
+
+		<h1>Concatenação</h1>
+		
+		<?php 
+			
+			$nome = 'Carlos';
+			$corPreferida = 'preto';
+			$idade = 20;
+			$hobby = 'jogar videogame';
+
+			echo 'Olá ' . $nome . ', ví que sua cor preferida é ' . $corPreferida . ', estou vendo também que você possui ' . $idade . ' anos e que gosta de ' . $hobby . '.'
+
+		?>
+
+	</body>
+</html>
