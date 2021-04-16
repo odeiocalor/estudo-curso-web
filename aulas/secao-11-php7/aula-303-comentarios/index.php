d21dffab62adfe1150900f8dd96a26ce9f134cce
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-16 17:27:21 -0300

    adição aula-303-comentarios

---
+++ aulas/secao-11-php7/aula-303-comentarios/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Comentários</title>
+	</head>
+	<body>
+
+		<h1>Comentários</h1>
+		
+		<?php 
+			// Comentário de uma linha
+
+			# Comentário de uma linha 
+
+			/* 
+				Comentário
+				de
+				várias
+				linhas
+			*/
+
+
+		?>
+
+	</body>
+</html>
