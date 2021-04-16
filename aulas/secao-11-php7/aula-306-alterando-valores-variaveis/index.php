23d1a7329d70f6ce0b94a5f7e5cd825b664a0867
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-16 17:50:51 -0300

    adição aula-306-alterando-valores-variaveis

---
+++ aulas/secao-11-php7/aula-306-alterando-valores-variaveis/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Variáveis</title>
+	</head>
+	<body>
+
+		<h1>Variáveis</h1>
+		
+		<?php 
+			
+			// string
+			$nome = 'Carlos Souza';
+
+			// int
+			$idade = 20;
+
+			// float
+			$peso = 61.5;
+
+			// bool
+			$fumante_sn = false;
+
+			// ... lógica ...
+
+			$idade = '30';
+
+		?>
+
+		<h1>Ficha cadastral</h1>
+		<br>
+		<p>Nome: <?= $nome ?></p>
+		<p>Idade: <?= $idade ?></p>
+		<p>Peso: <?= $peso ?></p>
+		<p>Fumante: <?= $fumante_sn ?></p>
+
+	</body>
+</html>
