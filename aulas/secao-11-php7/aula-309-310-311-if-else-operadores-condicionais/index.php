d7b329015d24c50c6d2e6d47971726f16d50f524
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-19 20:17:40 -0300

    adição aula-309-310-311-if-else-operadores-condicionais

---
+++ aulas/secao-11-php7/aula-309-310-311-if-else-operadores-condicionais/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>If/Else</title>
+	</head>
+	<body>
+
+		<h1>If/Else</h1>
+		
+		<?php 
+
+			echo 'Operadores condicionais:<br><br>';
+			// == Igual
+			// === Idêntico
+			// != ou <> Diferente
+			// !== Não idêntico
+			// < Menor que
+			// > Maior que
+			// <= Menor ou igual que
+			// >= Maior ou igual que
+
+			if (2 == 2) {
+				echo 'Verdadeiro';
+			} else {
+				echo 'Falso';
+			}
+
+		?>
+
+	</body>
+</html>
