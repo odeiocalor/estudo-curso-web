a72c1e62b2a0d14bbb7a11ef10ac102008f93ed7
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-19 23:50:32 -0300

    adição aula-323-operadores-aritmeticos-atribuicao-valores

---
+++ aulas/secao-11-php7/aula-323-operadores-aritmeticos-atribuicao-valores/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Operadores aritméticos na atribuição de valores</title>
+	</head>
+	<body>
+
+		<h1>Operadores aritméticos na atribuição de valores</h1>
+
+		<?php 
+
+			$x = 10;
+
+			// O mesmo pode ser feito com -, *, /, %
+			$x += 5;
+
+			echo $x;
+
+		?>
+
+	</body>
+</html>
