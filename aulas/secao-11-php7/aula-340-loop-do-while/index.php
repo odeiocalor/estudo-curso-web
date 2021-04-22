e68ab043c0333c8e26bb129a7b04eb28f88ff78c
odeiocalor <carlosalbertojuvino@gmail.com>
2021-04-22 18:53:54 -0300

    adição aula-340-loop-do-while

---
+++ aulas/secao-11-php7/aula-340-loop-do-while/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Loops - Do while</title>
+	</head>
+	<body>
+
+		<h1>Loops - Do while</h1>
+
+		<?php 
+
+			$x = 10;
+
+			do {
+				echo "X = $x";
+
+				// continue
+				// break
+			} while($x < 9);
+
+		?>
+
+	</body>
+</html>
