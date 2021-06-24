9d4fbdf5c9cdf1fb6fd7b73d75d2b5369126a158
odeiocalor <carlosalbertojuvino@gmail.com>
2021-06-24 20:01:39 -0300

    adição aula-377-tratamento-de-erro-customizado

---
+++ aulas/secao-12-php7-oo/aula-377-tratamento-de-erro-customizado/index.php
@@ -0,0 +1,23 @@
+<?php
+
+  class MinhaExceptionCustomizada extends Exception {
+    private $erro = '';
+
+    public function __construct($erro) {
+      $this->erro = $erro;
+    }
+
+    public function exibirMensagemErroCustomizada() {
+      echo '<div style="border: solid 1px #000; padding: 15px; background-color: red; color: white">';
+      echo $this->erro;
+      echo '</div>';
+    }
+  }
+
+  try {
+    throw new MinhaExceptionCustomizada('Esse é um erro de teste');
+  } catch(MinhaExceptionCustomizada $e) {
+    $e->exibirMensagemErroCustomizada();
+  }
+
+?>
